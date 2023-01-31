9f44dff190e75d51c971ef78622a2c1558bd24b9
cbowman422 <chrisb0wman@hotmail.com>
2023-01-31 15:46:49 -0500

    debug heroku

--- server.js
+++ server.js
@@ -51,9 +51,12 @@ app.use('/chat', chatController);
 const { Server } = require("socket.io");
 const io = new Server(http, {
     cors: {
-        origin: "https://capstone-chat.netlify.app/"
+        origin: "http://localhost:3000"
     }
   });
+  //origin: "https://capstone-chat.netlify.app/"
+
+
 
 //Add this before the app.get() block
 // on the sever instance bind to socket 'connection' event and provide socket as argument 
